042819a150c44996e5705d228b8e8973c023363b
Nnoey <610108020027@dpu.ac.th>
2020-09-16 15:30:35 +0700

    food

---
+++ food/app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const restaurantsRouter = require('./routes/restaurants');
+const app = express ();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/api/restaurants',restaurantsRouter);
+
+app.listen(port,'127.0.0.1',()=>{
+    console.log(`Listening to request on port ${port}`);
+});
---
+++ food/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        id:1,
+        name:'Potato Corner (โปเตโต้ คอร์เนอร์) - ถนนข้าวสาร',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLJEVDYC3JFL6/hero/182731a9214e4cb793fc6142442013ba_1600055633637784048.jpeg',
+        type:'Partner, Small Bites/Snacks'
+    },
+    {
+        id:2,
+        name:'KHIANG (เขียง) - ถนนพระอาทิตย์',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLGGETXRBV3A6/hero/253e8b332b804905b721ddf999d8d871_1594631339423966806.jpeg',
+        type:'Partner, Rice Bowls'
+    },
+    {
+        id:3,
+        name:'Subway (ซับเวย์) - ท่ามหาราช',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCFHAATNRDJCN/hero/0671a249f48145309f858b0d40a27db4_1600007885589659263.jpeg',
+        type:'Partner, Fast Food'
+    },
+    {
+        id:4,
+        name:'BonChon (บอนชอน) - ท่ามหาราช',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000021a/hero/6322e6fbb08a4002a6ca8ba998941a8a_1598580728800966917.png',
+        type:'Partner, Fried Chicken'
+    },
+    {
+        id:5,
+        name:'ตี๋เย็นตาโฟรสเด็ด - ศาลาว่าการกรุงเทพฯ',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMTKBDTT2XJAJ/hero/65641d0fcdc14d4a8961f56f0ead6fa5_1598847900883974941.jpeg',
+        type:'Partner, Noodles'
+    },
+    {
+        id:6,
+        name:'เก๋อาหารตามสั่ง - ถนนมหาจักร',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEXHF3KERJFGA/hero/da744dc7c52c481aa383aed9cd16b0b6_1589532114847970839.jpeg',
+        type:'Coupon, Partner, Cooked to Order'
+    },
+    {
+        id:7,
+        name:'KFC (เคเอฟซี) - ดิโอลด์สยาม พลาซ่า',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000007ad/hero/e2884691062d4d678794f738484060d7_1600007790046638981.jpeg',
+        type:'Partner, Fast Food, Fried Chicken'
+    },
+    {
+        id:8,
+        name:'Cafe Amazon (คาเฟ่ อเมซอน) - ถนนมหรรณพ',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEUGBXT2D2TJ/hero/776901be512444519763199d5fe9a272_1593408148178779119.jpeg',
+        type:'Partner, Coffee & Tea'
+    },
+    {
+        id:9,
+        name:'บุงกี่ เป็ดย่างฮ่องกง - บางลำพู',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4CJ2WAKAAZFE/hero/eac950d310ee457685bb98acf537d7ac_1576573337301905327.jpeg',
+        type:'Coupon, Partner, Chinese'
+    }
+];
+module.exports = data;
